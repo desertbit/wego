a462737130939d1643443844dfc652c72b3c4ac7
Sebastian <sebastian@desertbit.com>
2023-06-13 16:21:32 +0200

    update README

--- example/main.go
+++ example/main.go
@@ -19,49 +19,29 @@ func main() {
 	// Create client.
 	// The client will automatically login and renew its token regularly.
 	c, err := wego.NewClient(wego.Options{
-		RemoteAddr: "https://board.wahtari.io",
-		Username:   "webhooker",
-		Password:   "N1zHKfSMpEBqqNBzjXAAcXiW1Ey6rrzgRrvnRKhnn1U1dguzk4aGIiYfBvjDyOm1",
+		RemoteAddr: "https://your.wekanboard.com",
+		Username:   "user",
+		Password:   "secure-password",
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	const boardID = "GXCary4RDoJqR8n3u"
-	listIDs := []string{"8EXETWQ9YBThk7vjt", "kkhTPJDcLsSZ9hRYs"}
-	_ = listIDs
-	lists, err := c.GetAllLists(context.Background(), boardID)
+	boards, err := c.GetPublicBoards(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("lists: %+v\n", lists)
+	fmt.Printf("Public boards: %+v\n", boards)
 
-	u, err := c.GetComment(context.TODO(), boardID, "fN556xuzJFSEDCrn48k", "tinaer")
+	self, err := c.GetCurrentUser(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("user: %+v\n", u)
+	fmt.Printf("Self: %+v\n", self)
 
-	/*for _, listID := range listIDs {
-		cards, err := c.GetAllCards(context.Background(), boardID, listID)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("cards: %+v\n", cards)
-	}*/
-
-	/*card, err := c.GetCard(context.Background(), boardID, listIDs[1], "fN556xuzJFSEDC48k")
+	other, err := c.GetUser(context.Background(), "user-id-of-somebody")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("card: %+v\n", card)*/
-
-	r, err := c.EditCard(context.Background(), boardID, listIDs[1], "fN556xuzJFSEDC48k", wego.EditCardOptions{
-		ListID: listIDs[0],
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("response: %+v\n", r)
+	fmt.Printf("Other: %+v\n", other)
 }
